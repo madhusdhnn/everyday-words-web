159553d5f9a28f0de88b5f69f2607ddec21d94ff
madhusdhnn <madhusudhanan.p@gofrugal.com>
2020-05-19 23:39:51 +0530

    Hide human readable css class names for both global rules and custom rules

--- src/components/layouts/index.jsx
+++ src/components/layouts/index.jsx
@@ -1,27 +1,31 @@
 import React, {Component} from 'react';
 import '../../styles/main.scss';
 import theme from '../../theme';
-import {ThemeProvider} from '@material-ui/core/styles';
+import {createGenerateClassName, StylesProvider, ThemeProvider} from '@material-ui/core/styles';
 import Spinner from './Spinner';
 import SnackBar from './SnackBar';
 import NavBar from './NavBar';
 import Auth from '../containers/Auth';
+
+const generateClassName = createGenerateClassName({disableGlobal: true, productionPrefix: 'ewd-ttm'});
 
 const style = {width: '100%', marginBottom: 10};
 
 class Layout extends Component {
    render() {
       return (
-         <ThemeProvider theme={theme}>
-            <Spinner />
-            <SnackBar />
-            <Auth>
-               <NavBar />
-               <div style={style}>
-                  {this.props.children}
-               </div>
-            </Auth>
-         </ThemeProvider>
+         <StylesProvider generateClassName={generateClassName}>
+            <ThemeProvider theme={theme}>
+               <Spinner />
+               <SnackBar />
+               <Auth>
+                  <NavBar />
+                  <div style={style}>
+                     {this.props.children}
+                  </div>
+               </Auth>
+            </ThemeProvider>
+         </StylesProvider>
       );
    }
 }
